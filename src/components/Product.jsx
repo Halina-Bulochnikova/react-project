c43c6158d752958ab1290720331dbbc389d4f62c
halina_bulochnikova <yatsynkagalina@gmail.com>
2025-03-20 15:20:55 +0100

    add jsx files

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,11 +1,21 @@
+import { HiCurrencyDollar } from "react-icons/hi";
+
+
  function Product({ name, imgUrl, price }) {
     return (
       <div>
-        <h2>{name}</h2>
+        <h2 style={alertStyles}>{name}</h2>
       <img src={imgUrl} alt={name} width="480" />
-      <p>Price: {price} credits</p>
+      <p><HiCurrencyDollar className="my-icon" size={24} /> Price: {price} credits</p>
       </div>
     );
   };
-
+  const alertStyles = {
+    margin: 8,
+    padding: "12px 16px",
+    borderRadius: 4,
+    backgroundColor: "gray",
+    color: "white",
+  };
+  
 export default Product;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from '../src/components/App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "modern-normalize";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
+);
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
